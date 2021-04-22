caa771413fa1764336ece866071c8ef7ec1483f1
Brandon Lim <brandonlimchi@gmail.com>
2021-04-22 02:38:19 -0700

    skeleton code for main functions

---
+++ script.js
@@ -0,0 +1,48 @@
+function add(op1, op2)
+{
+    return op1 + op2;
+}
+
+function subtract(op1, op2)
+{
+    return op1 - op2;
+}
+
+function multiply(op1, op2)
+{
+    return op1 * op2;
+}
+
+function divide(op1, op2)
+{
+    if(op2 == 0)
+    {
+        //Run some error handling function for this
+    }
+    return op1 / op2;
+}
+
+function operate(op1, op2, operator)
+{
+    let result;
+    switch(operator)
+    {
+        case '+':
+            break;
+
+        case '-':
+            break;
+
+        case '*':
+            break;
+
+        case '/':
+            break;
+
+        default:
+            //Error handling function
+    }
+
+    //Print result to 'screen'
+    //Store the result somewhere for future computation
+}
